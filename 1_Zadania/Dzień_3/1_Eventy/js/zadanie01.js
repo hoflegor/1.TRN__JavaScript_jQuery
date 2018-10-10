3dd6519d9dc444ceb4aff66be44652237ef04394
hoflegor <tmsz.hof@gmail.com>
2018-10-11 01:53:47 +0200

    start with day 3

--- 1_Zadania/Dzień_3/1_Eventy/js/zadanie01.js
+++ 1_Zadania/Dzień_3/1_Eventy/js/zadanie01.js
@@ -1,9 +1,13 @@
-/**
- * Created by Jacek on 2016-01-12.
- */
 
-var menu = document.querySelector("#menu");
-var paragraf = document.querySelector("p");
+document.addEventListener('DOMContentLoaded', function() {
 
-menu.classList.add("menu");
-paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+    var menu = document.querySelector("#menu");
+    var paragraf = document.querySelector("p");
+//po odkomentowaniu tego co poniżej zadziała css z .menu
+// menu.classList.add("menu");
+    paragraf.innerText = "A to jest paragraf w zadaniu 1";
+
+});
+
+//Wniosek: Jeśli chcemy dodawać script w head a nie pod koniec body do trzeba cały kod js zamknąc w addEventListenera. Jeśli tego nie zrobimy i a script będzie w head to kod js będzie świrował gdyż  html jeszcze nie będzie wszystany (a z niego przeglądarka budej DOM na którym pracuje js).
+
--- 1_Zadania/Dzień_3/1_Eventy/js/zadanie02.js
+++ 1_Zadania/Dzień_3/1_Eventy/js/zadanie02.js
@@ -1,3 +1,14 @@
-/**
- * Created by Jacek on 2016-01-12.
- */
+document.addEventListener('DOMContentLoaded', function () {
+// event(w domyśle eventlistener) nastawiamy na rodzica, ale countera zmieniamy w dziecku :)
+
+    var parent = document.querySelector("#bubblingButtons");
+
+    parent.addEventListener("click", function (event)//dzięki podaniu tutaj zmiennej o dowolnej nazwie do niej własnie funkcja przypisze event, moze być to e, event, foo lub cokolwiek innego
+    {
+        console.log(event.target.tagName);
+        if (event.target.tagName === "BUTTON")//koniecznie z dużej litery
+        {
+            event.target.dataset.counter = parseInt(event.target.dataset.counter) + 1;
+        }
+    });
+});
